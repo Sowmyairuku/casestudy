33894bf9ec1570611b43ed2e3033dfef8a6d7681
Sowmyairuku <163623457+Sowmyairuku@users.noreply.github.com>
2025-10-24 00:10:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ def index():
         return render_template('reply.html', reply=reply_message)
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
